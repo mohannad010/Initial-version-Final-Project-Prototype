35743822cc2f216fa6724e2f0c9009cc826a2b6f
mohannad010 <63456312+mohannad010@users.noreply.github.com>
2023-07-01 12:01:15 -0400

    Create AutoBiography.js

---
+++ Pages/AutoBiography.js
@@ -0,0 +1,14 @@
+import React from "react";
+/** Components */
+import { View, Text } from "react-native";
+/** Styles */
+import containers from "../Styles/Containers";
+export default function AutoBiography() {
+  // header from the tab navigator
+  return (
+    <View style={containers.pageContainer}>
+      <Text>This page will your autoBiograph</Text>
+    </View>
+  );
+  // bottom menu from the tab navigator
+}
