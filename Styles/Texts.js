9ee53bcaa2442bb4afe3a44ca127f4b70346f80f
mohannad010 <63456312+mohannad010@users.noreply.github.com>
2023-07-01 12:02:59 -0400

    Add files via upload

---
+++ Styles/Texts.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const texts = StyleSheet.create({
+  privateInfoHeaderText: {
+    fontWeight: "bold",
+    fontSize: 20,
+    flex: 1,
+  },
+  singlePrivateInfoHeaderText: {
+    fontSize: 18,
+  },
+  familyTreeHeaderText: {
+    fontSize: 20,
+    fontFamily: "Comic Sans MS",
+  },
+});
+export default texts;
